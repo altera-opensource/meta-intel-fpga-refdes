a682f850305c0d33f4ba74b7f4e28071c5027799
Tien Hock Loh <thloh@altera.com>
2016-07-26 13:11:30 +0800

    FogBugz #382543: Fix GSRD initscript
    FPGA manager's sysfs path has changed from /sys/class/fpga_manager/fpga0/status
    to /sys/class/fpga_manager/fpga0/state, this patch addresses the API update.
    Also removes modprobe since Altera GPIO driver is no longer built as module, but
    as a built in kernel driver
    
    Signed-off-by: Tien Hock Loh <thloh@altera.com>

--- recipes-gsrd/altera-gsrd-initscripts/files/altera-gsrd-init.sh
+++ recipes-gsrd/altera-gsrd-initscripts/files/altera-gsrd-init.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+#  Copyright (C) 2016 Intel Corporation
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms and conditions of the GNU General Public License,
+#  version 2, as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # This script loads the gpio-altera module and the LED scrolling application,
 # updates the LCD with IP address obtained
 
-if [ "`cat /sys/class/fpga/fpga0/status`" == "user mode" ]; then
-	modprobe gpio-altera
-	if [ $? != 0 ]; then
-		echo "WARNING: Failed to load Altera GPIO module. \
-Web server and application for FPGA LED control will not work"
-	else
-		/www/pages/cgi-bin/scroll_server &
-	fi
+if [ "$(cat /sys/class/fpga_manager/fpga0/state)" == "operating" ]; then
+	/www/pages/cgi-bin/scroll_server &
 
 	modprobe altera_sysid
 	if [ $? != 0 ]; then

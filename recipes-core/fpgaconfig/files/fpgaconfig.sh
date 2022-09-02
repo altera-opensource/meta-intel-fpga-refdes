575b7310973c8a116989259fa3804d3b895da091
Teoh, Ji Sheng <ji.sheng.teoh@intel.com>
2022-09-02 05:23:30 +0000

    fpgaconfig: fix syntax error in script
    Signed-off-by: Teoh, Ji Sheng <ji.sheng.teoh@intel.com>

--- recipes-core/fpgaconfig/files/fpgaconfig.sh
+++ recipes-core/fpgaconfig/files/fpgaconfig.sh
@@ -15,6 +15,8 @@
 # This script loads a base FPGA device tree overlay to the kernel
 
 # Checks to ensure that we have DT overlay support
+MACHINE=`cat /etc/hostname`
+
 if [ ! -e /sys/kernel/config/device-tree/overlays ]; then
         echo "Error: Device Tree Overlay not supported"
         exit -1
@@ -22,60 +24,63 @@ fi
 
 # fpga_static_region
 if [ "$1" == "fpga_static_region.dtbo" ]; then
-	if [ "`cat /etc/hostname`" == "arria10" ]; then
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot -a fpga_static_region.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot -r fpga_static_region.dtbo
-		else
-			:
-		fi
-	else
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot/devicetree -a fpga_static_region.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot/devicetree -r fpga_static_region.dtbo
-		else
-			:
-		fi
+        if [ "$MACHINE" == "arria10" ]; then
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot -a fpga_static_region.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot -r fpga_static_region.dtbo
+                else
+                        :
+                fi
+        else
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot/devicetree -a ${MACHINE}_pr_fpga_static_region.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot/devicetree -r ${MACHINE}_pr_fpga_static_region.dtbo
+                else
+                        :
+                fi
+        fi
 fi
 
 # persona0
 if [ "$1" == "persona0.dtbo" ]; then
-	if [ "`cat /etc/hostname`" == "arria10" ]; then
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot -a persona0.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot -r persona0.dtbo
-		else
-			:
-		fi
-	else
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot/devicetree -a persona0.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot/devicetree -r persona0.dtbo
-		else
-			:
-		fi
+        if [ "$MACHINE" == "arria10" ]; then
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot -a persona0.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot -r persona0.dtbo
+                else
+                        :
+                fi
+        else
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot/devicetree -a ${MACHINE}_pr_persona0.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot/devicetree -r ${MACHINE}_pr_persona0.dtbo
+                else
+                        :
+                fi
+        fi
 fi
 
 # persona1
 if [ "$1" == "persona1.dtbo" ]; then
-	if [ "`cat /etc/hostname`" == "arria10" ]; then
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot -a persona1.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot -r persona1.dtbo
-		else
-			:
-		fi
-	else
-		if [ "$2" == "start" ]; then
-			dtbt -p /boot/devicetree -a persona1.dtbo
-		elif [ "$2" == "stop" ]; then
-			dtbt -p /boot/devicetree -r persona1.dtbo
-		else
-			:
-		fi
+        if [ "$MACHINE" == "arria10" ]; then
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot -a persona1.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot -r persona1.dtbo
+                else
+                        :
+                fi
+        else
+                if [ "$2" == "start" ]; then
+                        dtbt -p /boot/devicetree -a ${MACHINE}_pr_persona1.dtbo
+                elif [ "$2" == "stop" ]; then
+                        dtbt -p /boot/devicetree -r ${MACHINE}_pr_persona1.dtbo
+                else
+                        :
+                fi
+        fi
 fi

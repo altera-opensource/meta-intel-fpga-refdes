0be4bbc0f485c5923c150b4332f1d6cdd80e7370
drosdi <danish.ahmad.rosdi@intel.com>
2023-08-25 14:32:11 +0800

    Update agilex device-tree name to the latest agilex7 name
    Signed-off-by: drosdi <danish.ahmad.rosdi@intel.com>

--- recipes-core/fpgaconfig/files/fpgaconfig.sh
+++ recipes-core/fpgaconfig/files/fpgaconfig.sh
@@ -34,9 +34,9 @@ if [ "$1" == "fpga_static_region.dtbo" ]; then
                 fi
         elif [[ "$MACHINE" == "agilex7_dk_si_agf014e"* ]]; then
                 if [ "$2" == "start" ]; then
-                        dtbt -p /boot/devicetree -a agilex_pr_fpga_static_region.dtbo
+                        dtbt -p /boot/devicetree -a agilex7_pr_fpga_static_region.dtbo
                 elif [ "$2" == "stop" ]; then
-                        dtbt -p /boot/devicetree -r agilex_pr_fpga_static_region.dtbo
+                        dtbt -p /boot/devicetree -r agilex7_pr_fpga_static_region.dtbo
                 else
                         :
                 fi
@@ -65,9 +65,9 @@ if [ "$1" == "persona0.dtbo" ]; then
                 fi
         elif [[ "$MACHINE" == "agilex7_dk_si_agf014e"* ]]; then
                 if [ "$2" == "start" ]; then
-                        dtbt -p /boot/devicetree -a agilex_pr_persona0.dtbo
+                        dtbt -p /boot/devicetree -a agilex7_pr_persona0.dtbo
                 elif [ "$2" == "stop" ]; then
-                        dtbt -p /boot/devicetree -r agilex_pr_persona0.dtbo
+                        dtbt -p /boot/devicetree -r agilex7_pr_persona0.dtbo
                 else
                         :
                 fi
@@ -96,9 +96,9 @@ if [ "$1" == "persona1.dtbo" ]; then
                 fi
         elif [[ "$MACHINE" == "agilex7_dk_si_agf014e"* ]]; then
                 if [ "$2" == "start" ]; then
-                        dtbt -p /boot/devicetree -a agilex_pr_persona1.dtbo
+                        dtbt -p /boot/devicetree -a agilex7_pr_persona1.dtbo
                 elif [ "$2" == "stop" ]; then
-                        dtbt -p /boot/devicetree -r agilex_pr_persona1.dtbo
+                        dtbt -p /boot/devicetree -r agilex7_pr_persona1.dtbo
                 else
                         :
                 fi
